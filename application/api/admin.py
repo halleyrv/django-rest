166fc3e5985d3a03a06ffc148208d62ad839e653
halley rios <halley@lacafetalab.pe>
2020-05-28 13:12:48 -0500

    add mmodels into admin django

--- application/api/admin.py
+++ application/api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from api.models import Person, Task
+
+class TasksAdmin(admin.ModelAdmin):
+    list_display = ['name', 'status', 'person']
+
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+admin.site.register(Task, TasksAdmin)
+admin.site.register(Person, PersonAdmin)
