927a8afe06bbd133d229c5e556046e9797af48f7
halley rios <halley@lacafetalab.pe>
2020-05-28 13:07:52 -0500

    create models task and person

--- application/api/models.py
+++ application/api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=70)
+
+    class Meta:
+        db_table = "hr_person"
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='tasks')
+    name = models.CharField(max_length=70)
+    status = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = "hr_task"
+
+    def __str__(self):
+        return self.name
